6c23eca0e951d7454a027aa805705a2c3ff07202
dineshch3 <dineshch3@gmail.com>
2017-07-15 13:16:07 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(int i=1;i<=100000;i++){  
+        System.out.println(i);  
+    } 
     }
 }
